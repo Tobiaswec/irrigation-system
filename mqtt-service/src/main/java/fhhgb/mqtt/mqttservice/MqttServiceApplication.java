468d94650ac29b2b9754de93ffb80b77fb6ede68
Tobiaswec <wecht1998@gmail.com>
2022-06-09 19:30:16 +0200

    add missing files

---
+++ mqtt-service/src/main/java/fhhgb/mqtt/mqttservice/MqttServiceApplication.java
@@ -0,0 +1,13 @@
+package fhhgb.mqtt.mqttservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttServiceApplication.class, args);
+	}
+
+}
